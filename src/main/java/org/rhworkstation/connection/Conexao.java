ca1db4c2330c612b32a44d1753503fc6b086aa7c
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-09-22 19:48:43 -0300

    feat: cria a conexao com raiÃlway

--- src/main/java/org/rhworkstation/connection/Conexao.java
+++ src/main/java/org/rhworkstation/connection/Conexao.java
@@ -1,4 +1,17 @@
 package org.rhworkstation.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    private static final String URL = "mysql://root:ZyYLbrJapvRAhZGPpzJkSzkDhofQomoQ@maglev.proxy.rlwy.net:26517/railway";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ZyYLbrJapvRAhZGPpzJkSzkDhofQomoQ@maglev";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
+
+
