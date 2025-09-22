55b8ae8b2d7f56bc388c8154bc890240fdfe4a9e
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-22 20:20:58 -0300

    feat: Adiciona model (#19)
    Código correto

--- src/main/java/org/rhworkstation/model/Admin.java
+++ src/main/java/org/rhworkstation/model/Admin.java
@@ -1,4 +1,59 @@
 package org.rhworkstation.model;
 
+import java.time.LocalDateTime;
+
 public class Admin {
+
+    private int id;
+    private String nome;
+    private String emai;
+    private String senha;
+    private LocalDateTime ultimo_login;
+
+    public Admin() {}
+
+    public Admin(int id, String nome, String emai, String senha, LocalDateTime ultimo_login) {
+        this.id = id;
+        this.nome = nome;
+        this.emai = emai;
+        this.senha = senha;
+        this.ultimo_login = ultimo_login;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getUltimo_login() {
+        return ultimo_login;
+    }
+    public void setUltimo_login(LocalDateTime ultimo_login) {
+        this.ultimo_login = ultimo_login;
+    }
+
 }
---
+++ src/main/java/org/rhworkstation/model/Colaborador.java
@@ -0,0 +1,75 @@
+package org.rhworkstation.model;
+
+public class Colaborador {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String cargo;
+    private String departamento;
+    private double salario_hora;
+
+    public Colaborador() {}
+
+    public Colaborador(int id, String nome, String cpf, String email, String cargo, String departamento, double salario_hora) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.cargo = cargo;
+        this.departamento = departamento;
+        this.salario_hora = salario_hora;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double getSalario_hora() {
+        return salario_hora;
+    }
+    public void setSalario_hora(double salario_hora) {
+        this.salario_hora = salario_hora;
+    }
+
+}
