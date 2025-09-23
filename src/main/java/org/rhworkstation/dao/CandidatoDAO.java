573434d7330be67254ae5c5b88ffb37667b5f39e
nicoo.franca <nicollas_franca@estudante.sesisenai.org.br>
2025-09-23 19:52:14 -0300

    Feat/candidato (#35)
    * feat: arruma class Curriculo
    
    * #22 feat: cria metodo criarCandidato na CandidatoDAO

--- src/main/java/org/rhworkstation/dao/CandidatoDAO.java
+++ src/main/java/org/rhworkstation/dao/CandidatoDAO.java
@@ -1,4 +1,26 @@
 package org.rhworkstation.dao;
 
+import org.rhworkstation.connection.Conexao;
+import org.rhworkstation.model.Candidato;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CandidatoDAO {
+    public void criarCandidato(Candidato candidato) throws SQLException {
+        String query = "INSERT INTO candidato (nome, cpf, email) VALUES (?, ?, ?)";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+            stmt.setString(1, candidato.getNome());
+            stmt.setString(2, candidato.getCpf());
+            stmt.setString(3, candidato.getEmail());
+            stmt.executeUpdate();
+
+            System.out.println("Candidato criado com sucesso!");
+
+        }
+    }
 }
+//Olhar Minhas vagas
