6a6112cddc8c2e4428cea9c17627a344dec2aef8
nicoo.franca <nicollas_franca@estudante.sesisenai.org.br>
2025-09-23 21:03:04 -0300

    #38 feat: cria inputs padroes (#41)

--- src/main/java/org/rhworkstation/view/Inputs.java
+++ src/main/java/org/rhworkstation/view/Inputs.java
@@ -1,4 +1,142 @@
 package org.rhworkstation.view;
 
+import org.rhworkstation.model.enums.StatusVaga;
+
+import java.util.Scanner;
+
 public class Inputs {
+    Scanner input = new Scanner(System.in);
+
+    public String inputNome(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Nome                          ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String nome = input.nextLine();
+
+        return nome;
+    }
+
+    public String inputCpf(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o CPF                           ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String cpf = input.nextLine();
+
+        return cpf;
+    }
+
+    public String inputEmail(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o email                         ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String email = input.nextLine();
+
+        return email;
+    }
+
+    public String inputSenha(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Senha                         ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String senha = input.nextLine();
+
+        return senha;
+    }
+    public String inputCargo(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Cargo                         ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String cargo = input.nextLine();
+
+        return cargo;
+    }
+
+    public String inputDepartamento(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Departamento                  ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String departamento = input.nextLine();
+
+        return departamento;
+    }
+
+    public double inputSalarioHora(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Valor da Hora                 ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        double salarioHora = input.nextDouble();
+
+        return salarioHora;
+    }
+
+    public int inputIdade(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite a Idade                         ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        int idade = input.nextInt();
+
+        return idade;
+    }
+
+    public String inputSexo(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Sexo                          ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String sexo = input.nextLine();
+
+        return sexo;
+    }
+
+    public String inputFormacao(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite a Formação                      ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String formacao = input.nextLine();
+
+        return formacao;
+    }
+
+    public String inputTexto(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o texto                         ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String texto = input.nextLine();
+
+        return texto;
+    }
+
+    public String inputNomeVaga(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Nome da Vaga                  ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String nomeVaga = input.nextLine();
+
+        return nomeVaga;
+    }
+    public String inputDescricaoVaga(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite a Descrição da Vaga             ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        String descricaoVaga = input.nextLine();
+
+        return descricaoVaga;
+    }
+
+    public double inputSalarioHoraVaga(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Salario por Hora da Vaga      ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        double SalarioHoraVaga = input.nextDouble();
+
+        return SalarioHoraVaga;
+    }
+
+    public StatusVaga status(){
+        System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        System.out.println("┃ - Digite o Status da Vaga                ┃");
+        System.out.print  ("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n:");
+        StatusVaga status = StatusVaga.valueOf(input.next());
+
+        return status;
+    }
 }
