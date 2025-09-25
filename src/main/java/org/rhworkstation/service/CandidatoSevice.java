70d3a4f08e28729a806331e7def53bec878e0846
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-09-25 19:27:30 -0300

    #22 feat: cria funcao criarCandidato

--- src/main/java/org/rhworkstation/service/CandidatoSevice.java
+++ src/main/java/org/rhworkstation/service/CandidatoSevice.java
@@ -1,4 +1,29 @@
 package org.rhworkstation.service;
 
+import org.rhworkstation.dao.CandidatoDAO;
+import org.rhworkstation.model.Candidato;
+import org.rhworkstation.view.Inputs;
+
+import java.sql.SQLException;
+
 public class CandidatoSevice {
+
+    Inputs data = new Inputs();
+
+    public void criarUsuario(){
+
+        String nome = data.inputNome();
+        String cpf = data.inputCpf();
+        String email = data.inputEmail();
+        String senha = data.inputSenha();
+
+        var novoCandidato = new Candidato(nome, cpf, email, senha);
+        var candidatoDAO = new CandidatoDAO();
+
+        try {
+            candidatoDAO.criarCandidato(novoCandidato);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
