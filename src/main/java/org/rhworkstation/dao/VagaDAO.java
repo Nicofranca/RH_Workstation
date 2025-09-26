5ba46505602e128045650ac75bdcee32c02107b0
sara25097 <sara_soares-santos@estudante.sesisenai.org.br>
2025-09-26 14:01:09 -0300

    Feat/cria olhar vagas (#49)
    * #40 feat: cria metodo para visualização de vagas
    
    * #40 refactor: Apaga a visualização do id do método visualizar vaga

--- src/main/java/org/rhworkstation/dao/VagaDAO.java
+++ src/main/java/org/rhworkstation/dao/VagaDAO.java
@@ -1,4 +1,41 @@
 package org.rhworkstation.dao;
 
+import org.rhworkstation.connection.Conexao;
+import org.rhworkstation.model.Vaga;
+import org.rhworkstation.model.enums.StatusVaga;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VagaDAO {
+
+
+    public List<Vaga> listarVagas() {
+        List<Vaga> vagasDisponiveis = new ArrayList<>();
+        String sql = "SELECT * FROM vagas";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                String nomeVaga = rs.getString("nomeVaga");
+                String descricao = rs.getString("descricao");
+                double salarioHora = rs.getDouble("salarioHora");
+                StatusVaga status = StatusVaga.valueOf(rs.getString("status"));
+
+                Vaga vaga = new Vaga(nomeVaga, descricao, salarioHora, status);
+                vagasDisponiveis.add(vaga);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar vagas disponíveis: " + e.getMessage());
+        }
+
+        return vagasDisponiveis;
+    }
 }
