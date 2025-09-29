be2e110f08a8962355e0b7640e7fff98ea7c6ec5
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2025-09-29 18:52:36 -0300

    #26 feat: exclui curriculo atraves do cpf

--- src/main/java/org/rhworkstation/dao/CandidatoDAO.java
+++ src/main/java/org/rhworkstation/dao/CandidatoDAO.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CandidatoDAO {
+public class  CandidatoDAO {
     public void criarCandidato(Candidato candidato) throws SQLException {
         String query = "INSERT INTO candidato (nome, cpf, email, senha) VALUES (?, ?, ?, ?)";
 
@@ -25,30 +25,26 @@ public class CandidatoDAO {
         }
     }
 
-    public Candidato buscarPorCPF(Candidato candidato) throws SQLException{
-        String query = "SELECT id from candidato WHERE cpf = (?)";
+    public int buscarPorCPF(String CPFCandidato) throws SQLException{
+        String query = "SELECT id FROM candidato WHERE cpf = (?)";
 
         Candidato candidatoEncontrado = null;
 
         try(Connection conn = Conexao.conectar();
         PreparedStatement stmt = conn.prepareStatement(query)) {
 
-            stmt.setString(1, candidato.getCpf());
+            stmt.setString(1, CPFCandidato);
 
             try(ResultSet rs = stmt.executeQuery()) {
                 if(rs.next()){
                     candidatoEncontrado = new Candidato();
 
                     candidatoEncontrado.setId(rs.getInt("id"));
-                    candidatoEncontrado.setNome(rs.getString("nome"));
-                    candidatoEncontrado.setCpf(rs.getString("cpf"));
-                    candidatoEncontrado.setEmail(rs.getString("email"));
-                    candidatoEncontrado.setSenha(rs.getString("senha"));
                 }
             }
         }
 
-        return candidatoEncontrado;
+        return candidatoEncontrado.getId();
     }
 }
 
