61eb9208a06191287f98a1c1e68b9862cef029a9
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-29 21:51:29 -0300

    feat: Cria as classes relacionadas a FolhaSalarial

---
+++ src/main/java/org/rhworkstation/model/FolhaSalarial.java
@@ -0,0 +1,65 @@
+package org.rhworkstation.model;
+
+public class FolhaSalarial {
+
+    private int id;
+    private String cpf_colaborador;
+    private double salario_bruto;
+    private double inss;
+    private double salario_liquido;
+
+    public FolhaSalarial(int id, String cpf_colaborador, double salario_bruto, double inss, double salario_liquido) {
+        this.id = id;
+        this.cpf_colaborador = cpf_colaborador;
+        this.salario_bruto = salario_bruto;
+        this.inss = inss;
+        this.salario_liquido = salario_liquido;
+    }
+
+    public FolhaSalarial(String cpf_colaborador, double salario_bruto, double inss, double salario_liquido) {
+        this.cpf_colaborador = cpf_colaborador;
+        this.salario_bruto = salario_bruto;
+        this.inss = inss;
+        this.salario_liquido = salario_liquido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf_colaborador() {
+        return cpf_colaborador;
+    }
+
+    public void setCpf_colaborador(String cpf_colaborador) {
+        this.cpf_colaborador = cpf_colaborador;
+    }
+
+    public double getSalario_bruto() {
+        return salario_bruto;
+    }
+
+    public void setSalario_bruto(double salario_bruto) {
+        this.salario_bruto = salario_bruto;
+    }
+
+    public double getInss() {
+        return inss;
+    }
+
+    public void setInss(double inss) {
+        this.inss = inss;
+    }
+
+    public double getSalario_liquido() {
+        return salario_liquido;
+    }
+
+    public void setSalario_liquido(double salario_liquido) {
+        this.salario_liquido = salario_liquido;
+    }
+}
---
+++ src/main/java/org/rhworkstation/service/FolhaSalarialService.java
@@ -0,0 +1,9 @@
+package org.rhworkstation.service;
+
+public class FolhaSalarialService {
+
+    public void LiberarFolhaSalarial(){
+
+
+    }
+}
